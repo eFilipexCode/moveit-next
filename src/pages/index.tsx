7d807315ecc7de0ce5b2b52b0d78666e67746636
Emanuel Filipe <efilipecode@gmail.com>
2021-02-26 13:47:53 -0300

    Add favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,14 @@ import CountDown from '../components/CountDown';
 import ExperienceBar from '../components/ExperienceBar';
 import Profile from '../components/Profile';
 import styles from '../styles/pages/Home.module.css';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div className={`${styles.container}`}>
+      <Head>
+        <title>Início | Moveit</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div>
