a5d760bc7815e9d503bff81584840704d8b2ef02
Emanuel Filipe <efilipecode@gmail.com>
2021-02-28 15:42:50 -0300

    Updates

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,9 @@
 import styles from '../styles/components/Profile.module.css';
+import { ChallengesContext } from '../contexts/ChallengeContexts';
+import { useContext } from 'react';
 
 export default function Profile() {
+    const {level} = useContext(ChallengesContext);
     return (
         <div className={`${styles.profileContainer}`}>
             <img src="https://github.com/efilipecode.png" alt="Emanuel Filipe" />
@@ -8,7 +11,7 @@ export default function Profile() {
                 <strong>Emanuel Filipe</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
